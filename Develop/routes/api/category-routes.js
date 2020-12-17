dc59a548139d6dcbe46cec1b59802a27423cb379
Robert Schmahl <rschm007@gmail.com>
2020-12-17 15:55:49 -0800

    category.js defined columns

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,25 +3,36 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+// GET all categories
   // find all categories
   // be sure to include its associated Products
+router.get('/', async (req, res) => {
+try {
+  const categoryData = await Category.findAll();
+  res.status(200).json(categoryData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
-router.get('/:id', (req, res) => {
+// GET a single category
   // find one category by its `id` value
   // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
 });
 
-router.post('/', (req, res) => {
+// CREATE a category
+router.post('/', async (req, res) => {
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+// UPDATE a category
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+// DELETE all categories
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
 });
 
