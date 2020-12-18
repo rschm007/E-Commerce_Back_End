c23198846bac6b0919e53056159c450daff90065
Robert Schmahl <rschm007@gmail.com>
2020-12-17 16:16:29 -0800

    corrected columns for product model

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,13 +1,12 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
-const { Category } = require('.');
+const { Model, DataTypes } = require("sequelize");
+const { Category } = require(".");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
 Product.init(
   {
     id: {
@@ -16,14 +15,23 @@ Product.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    {
-      product_name: DataTypes.STRING,
-      price: DataTypes.DECIMAL(10, 2),
-      stock: DataTypes.INTEGER,
-      references: {
-        model: 'Category',
-        key: 'category_name'
-      }
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+    },
+    references: {
+      //     * References the `Category` model's `category_name`.
+      model: "category",
+      key: "category_name",
     },
   },
   {
@@ -31,7 +39,7 @@ Product.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 
